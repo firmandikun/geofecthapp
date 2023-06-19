ffd5178426e7128eaa3b7f607e633df554188599
firman <firman.technopartner@gmail.com>
2023-06-19 22:37:51 +0700

    frist app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import SplashScreen from "./views/SplashScreen";
+
+
 
 function App() {
+
+  const [showSplash, setShowSplash] = useState(true);
+
+useEffect(() => {
+  // Simulate an asynchronous operation (e.g., loading data)
+  setTimeout(() => {
+    setShowSplash(false); // Hide the splash screen after a certain duration
+  }, 3000); // Adjust the duration (in milliseconds) as needed
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        showSplash ? <SplashScreen /> :   <iframe
+        src="https://tesgpsterbaru.000webhostapp.com/"
+        width="100%"
+        style={{ height: '100vh' }}
+        title="Web Page"
+      />
+      }
+    
     </div>
   );
 }
---
+++ src/views/SplashScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen" style={{ height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#87ceeb'}} >
+      {/* splash screen content here */}
+     <div>
+     <h2 style={{ textAlign: 'center', color: 'white' }} > APP Geofething </h2>
+     </div>
+
+    </div>
+  );
+};
+
+export default SplashScreen;
